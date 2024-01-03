4948115cda7046055ab2932ab5a72503c4f47b30
Datafox-JCP <datafox@outlook.com>
2024-01-03 12:22:45 -0600

    Initial Commit

---
+++ Stefanini/StefaniniApp.swift
@@ -0,0 +1,17 @@
+//
+//  StefaniniApp.swift
+//  Stefanini
+//
+//  Created by Juan Hernandez Pazos on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StefaniniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
